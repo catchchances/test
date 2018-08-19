3f930cc9df3eb740421c670905e9008db3e8034d
bbbbln <hello@zhanglong.info>
2018-08-19 22:50:52 +0800

    同步与非同步的区别

---
+++ src/main/java/com/kkdz/test/multi_thread/sync/TransferRunnable.java
@@ -0,0 +1,32 @@
+package com.kkdz.test.multi_thread.sync;
+
+public class TransferRunnable implements Runnable {
+
+	private Bank bank;
+
+	private int fromAccount;
+
+	private double maxAmount;
+
+	private int DELAY = 10;
+
+	public TransferRunnable(Bank bank, int from, double max) {
+		this.bank = bank;
+		this.fromAccount = from;
+		this.maxAmount = max;
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				int toAmount = (int) (bank.size() * Math.random());
+				double amount = this.maxAmount * Math.random();
+				bank.transfer(this.fromAccount, toAmount, amount);
+				Thread.sleep(this.DELAY);
+			}
+		} catch (InterruptedException e) {
+		}
+	}
+
+}
---
+++ src/main/java/com/kkdz/test/multi_thread/unsync/UnsyncBankTest.java
@@ -0,0 +1,16 @@
+package com.kkdz.test.multi_thread.unsync;
+
+public class UnsyncBankTest {
+
+	public static void main(String[] args) {
+		int nAcounts = 100;
+		double initialBalance = 1000;
+		Bank b = new Bank(nAcounts, initialBalance);
+		for (int i = 0; i < nAcounts; i++) {
+			TransferRunnable r = new TransferRunnable(b, i, initialBalance);
+			Thread t = new Thread(r);
+			t.start();
+		}
+	}
+
+}
