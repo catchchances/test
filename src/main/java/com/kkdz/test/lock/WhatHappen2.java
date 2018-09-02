66d66df5ae0aa8af26a48265dbf36f900285e755
bbbbln <hello@zhanglong.info>
2018-09-02 13:45:48 +0800

    volatile和obj.wait()的原理

--- src/main/java/com/kkdz/test/lock/WhatHappen2.java
+++ src/main/java/com/kkdz/test/lock/WhatHappen2.java
@@ -2,60 +2,38 @@ package com.kkdz.test.lock;
 
 public class WhatHappen2 {
 
-	//不是一个好的例子
+	// 结果表明：每个线程都会进入锁，重新开始执行代码。
 	public static void main(String[] args) {
 
-		Object obj = new Object();
-		WaitAction wa1 = new WaitAction(obj);
-		WaitAction wa2 = new WaitAction(obj);
-		NotifyAction na1 = new NotifyAction(obj);
-		NotifyAction na2 = new NotifyAction(obj);
-		wa1.start();
-		wa2.start();
-		na1.start();
-		na2.start();
+		WaitResult wr = new WaitResult();
+		for (int i = 0; i < 10; i++) {
+			new Thread(wr).start();
+
+		}
 	}
 
 }
 
-class WaitAction extends Thread {
-	private Object obj;
-
-	public WaitAction(Object obj) {
-		this.obj = obj;
-	}
+class WaitResult extends Thread {
+	private Object obj = new Object();
 
 	@Override
 	public void run() {
 
+		// 因为obj会不断的让线程添加到等待队列里。所以下一个线程如果接着走while处的代码，就不会输出before...语句。
+		// 如果，新的线程重新获取锁，从头开始执行，就会输出大量的before...代码
+		// 结果表明：每个线程都会进入锁，重新开始执行代码。
 		synchronized (obj) {
 			System.out.printf("I am %s and running between synchronized block before obj.wait() \n",
 					Thread.currentThread().getName());
 			try {
-				obj.wait();
+				while (true) {
+					obj.wait();
+				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			System.out.printf("I am %s and running after obj.wait() \n", Thread.currentThread().getName());
 		}
 	}
-
-}
-
-class NotifyAction extends Thread {
-	private Object obj;
-
-	public NotifyAction(Object obj) {
-		this.obj = obj;
-	}
-
-	@Override
-	public void run() {
-		synchronized (obj) {
-			System.out.printf("I am %s and before obj.notifyAll() \n", Thread.currentThread().getName());
-			obj.notifyAll();
-			System.out.printf("I am %s and after obj.notifyAll() \n", Thread.currentThread().getName());
-		}
-	}
-
 }
