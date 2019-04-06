6c8940aafc2c804f1a31137b1c4e9e5c3d0da9d9
bbbbln <bbbbln@bbbbln-VirtualBox>
2019-04-06 14:54:31 +0800

    bio socket demo

---
+++ src/main/java/com/kkdz/test/io/bio/TimeServerHandler.java
@@ -0,0 +1,69 @@
+package com.kkdz.test.io.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Date;
+
+public class TimeServerHandler implements Runnable {
+
+	private Socket socket = null;
+	
+	public TimeServerHandler(Socket socket) {
+		this.socket = socket;
+	}
+
+	@Override
+	public void run() {
+		BufferedReader in = null;
+		PrintWriter out = null;
+		try {
+			in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+			out = new PrintWriter(this.socket.getOutputStream(), true);
+			String currentTime = null;
+			String body = null;
+			while(true) {
+				body = in.readLine();
+				if(null == body)
+				{
+					break;
+				}
+				System.out.println("this time server receive order:"+body);
+				currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+				out.println(currentTime);
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			if(null != in) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				in = null;
+			}
+			if(null != out) {
+				out.close();
+				out = null;
+			}
+			if(null != socket) {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				socket = null;
+			}
+		}
+	}
+
+
+}
